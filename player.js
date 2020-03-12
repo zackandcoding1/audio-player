09cc8897f536b73c27a0b6f5e4107cca206ff39c
Isaac <isaacnbgomes@gmail.com>
2020-03-12 11:57:17 -0300

    Update player.js

--- player.js
+++ player.js
@@ -30,7 +30,7 @@ export default {
     },
     toggleMute() {
         this.audio.muted = !this.audio.muted;
-        this.mute.innerText = this.audio.muted ? "volume_down" : "volume_up";
+        this.mute.innerText = this.audio.muted ? "volume_mute" : "volume_up";
         // ^ muda o ícone de áudio para mudo e vice-versa
     },
     next() {
